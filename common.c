cc4344ef851211d4759340809d63e4e2c509b37f
Massimo Lauria <lauria.massimo@gmail.com>
2011-01-09 18:09:26 +0100

    Naive algorithm for black pebbled, finished (2)

---
+++ common.c
@@ -0,0 +1,23 @@
+/*
+   Copyright (C) 2010, 2011 by Massimo Lauria <lauria.massimo@gmail.com>
+
+   Created   : "2011-01-09, domenica 16:30 (CET) Massimo Lauria"
+   Time-stamp: "2011-01-09, domenica 16:31 (CET) Massimo Lauria"
+
+   Description::
+
+   Common function for debug and housekeeping purposes.
+
+
+*/
+
+/* Preamble */
+#include <stdlib.h>
+#include "common.h"
+
+
+
+/* Code */
+void debug_trap_bait() {
+  return;
+}
