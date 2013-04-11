6225a5b2d081f92c082bed14561340847b72a4be
Massimo Lauria <lauria.massimo@gmail.com>
2013-04-12 01:34:25 +0200

    Better error messages

--- kthparser.c
+++ kthparser.c
@@ -2,7 +2,7 @@
    Copyright (C) 2013 by Massimo Lauria <lauria.massimo@gmail.com>
 
    Created   : "2013-04-11, 21:15 (CEST) Massimo Lauria"
-   Time-stamp: "2013-04-12, 01:26 (CEST) Massimo Lauria"
+   Time-stamp: "2013-04-12, 01:33 (CEST) Massimo Lauria"
 
    Description::
 
@@ -55,7 +55,7 @@ DAG *kthparser(FILE *input) {
     if (temp==-1) break;
     
     if (buffer[0]=='c') { /* Comment line */
-      fprintf(stderr,"comment line\n");
+      fprintf(stderr,"PARSER: comment line\n");
       do { temp=getc(input); } while(temp!=-1 && temp!='\n');
       continue;
     }
@@ -68,7 +68,7 @@ DAG *kthparser(FILE *input) {
       
       vertices = read;
       position = 0;
-      fprintf(stderr,"GOOD: vertex number %u\n",read);
+      fprintf(stderr,"PARSER: there are %u vertices.\n",read);
 
       indegree = (size_t*)calloc(vertices,sizeof(size_t));
       outdegree = (size_t*)calloc(vertices,sizeof(size_t));
@@ -81,12 +81,12 @@ DAG *kthparser(FILE *input) {
     } else if ( read < position ) { /* Still reading vertex specification */
       
       if (last_predecessor >= read) {
-        fprintf(stderr,"ERROR: predecessors must be in ascending order (%u >= %u)\n",
+        fprintf(stderr,"PARSER ERROR: predecessors must be in ascending order.\n",
                 last_predecessor,read);
         exit(-1);
       }
 
-      fprintf(stderr,"GOOD: %u preceeds %u\n",read,position);
+      fprintf(stderr,"PARSER: Edge %u -> %u\n",read,position);
       last_predecessor = read;
       
       indegree[position-1]++;
@@ -99,16 +99,16 @@ DAG *kthparser(FILE *input) {
       insertpoint->next = NULL;
 
     } else if (read == position ){
-      fprintf(stderr,"ERROR: self loop on vertex %u (b:%s)\n",position,buffer);
+      fprintf(stderr,"PARSER ERROR: self loop on vertex %u.\n",position);
       exit(-1);
  
     } else if (read > vertices ){
-      fprintf(stderr,"ERROR: too large vertex %u\n",read);
+      fprintf(stderr,"PARSER ERROR: out of range vertex %u.\n",read);
       exit(-1);
 
     } else if (read == position+1) { /* This is a vertex specification. Pass the : */
     
-      fprintf(stderr,"GOOD: Read specification for %u\n",read);
+      fprintf(stderr,"PARSER: start reading predecessors of %u.\n",read);
       
       last_predecessor = 0;
       position = position +1;
@@ -116,22 +116,21 @@ DAG *kthparser(FILE *input) {
 
       temp=fscanf(input,"%100s",&buffer);
       if (temp==-1 || strcmp(buffer,":")!=0) {
-        fprintf(stderr,"ERROR: expecting : specification for vertex %u (b:%s)\n",position,buffer);
+        fprintf(stderr,"PARSER ERROR: expecting : before predecessors for vertex %u.\n",position,buffer);
         exit(-1);
       }
-      fprintf(stderr,"GOOD: Read : from %s\n",buffer);
 
     } else {
-      fprintf(stderr,"ERROR: missing vertex %u (b:%s)\n",position+1,buffer);
+      fprintf(stderr,"PARSER ERROR: Vertex %u missing.\n",position+1);
       exit(-1);
     }
   }
 
   if (vertices==0)
-    fprintf(stderr,"ERROR: positive number of vertices expected (b:%s)\n",buffer);
+    fprintf(stderr,"PARSER ERROR: positive number of vertices expected.\n");
  
   if (position != vertices) {
-    fprintf(stderr,"ERROR: unexpected end of file.",vertices); 
+    fprintf(stderr,"PARSER ERROR: unexpected end of file at vertex %u.\n",position); 
   }
 
 
