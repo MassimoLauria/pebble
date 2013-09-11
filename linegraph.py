d6960d781cfa38aa7323566cb53358fc48f9b93b
Massimo Lauria <lauria.massimo@gmail.com>
2013-09-11 15:23:34 +0200

    Line graphs start from vertex 1

--- linegraph.py
+++ linegraph.py
@@ -4,12 +4,12 @@ import sys
 
 N=int(sys.argv[1])
 
-print "c line graph with %d vertices" % N
-print N
+print "c line graph of length %d (with %d vertices)" % (N,N+1)
+print N+1
 
 
-if N > 0:
+if N >= 0:
     print "1 :"
 
-for i in range(2,N+1):
+for i in range(2,N+2):
     print "%d : %d" % (i,i-1)
